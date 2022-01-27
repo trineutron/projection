5a0459321cb72996b22941876ca22c8a6cd4816a
trineutron <programgoo@gmail.com>
2022-01-28 01:55:06 +0900

    docstring、変数名の変更

--- src/polynomial.py
+++ src/polynomial.py
@@ -1,3 +1,4 @@
+"""Optimize projection method."""
 import math
 from itertools import product
 
@@ -9,48 +10,49 @@ N = 10
 
 
 def calc_error(mat):
+    """Calculate strain."""
     sum_error = 0.0
     weight = 0.0
-    for i, j in product(range(2 * N), range(N)):
-        longitude = (i + 0.5) / (2 * N) * math.pi
-        latitude = (j + 0.5) / (2 * N) * math.pi
+    for idx_longitude, idx_latitude in product(range(2 * N), range(N)):
+        longitude = (idx_longitude + 0.5) / (2 * N) * math.pi
+        latitude = (idx_latitude + 0.5) / (2 * N) * math.pi
         cos_lat = math.cos(latitude)
         weight += cos_lat
 
         dfdx = 0.0
         dgdy = 0.0
-        for k in reversed(range(DEGREE)):
+        for i in reversed(range(DEGREE)):
             sum_dfdx = 0.0
             sum_dgdy = 0.0
-            for l in reversed(range(DEGREE)):
+            for j in reversed(range(DEGREE)):
                 sum_dfdx *= latitude*latitude
                 sum_dgdy *= latitude*latitude
-                sum_dfdx += mat[2 * (k * DEGREE + l)]
-                sum_dgdy += mat[2 * (k * DEGREE + l) + 1] * (2*l+1)
+                sum_dfdx += mat[2 * (i * DEGREE + j)]
+                sum_dgdy += mat[2 * (i * DEGREE + j) + 1] * (2*j+1)
             dfdx *= longitude*longitude
-            dfdx += sum_dfdx * (2*k+1)
+            dfdx += sum_dfdx * (2*i+1)
             dgdy *= longitude*longitude
             dgdy += sum_dgdy
         dfdx /= cos_lat
 
         dfdy = 0.0
-        for k in reversed(range(DEGREE)):
+        for i in reversed(range(DEGREE)):
             sum_line = 0.0
-            for l in reversed(range(1, DEGREE)):
+            for j in reversed(range(1, DEGREE)):
                 sum_line *= latitude*latitude
-                sum_line += mat[2 * (k * DEGREE + l)] * (2*l)
+                sum_line += mat[2 * (i * DEGREE + j)] * (2*j)
             dfdy *= longitude*longitude
             dfdy += sum_line
         dfdy *= longitude * latitude
 
         dgdx = 0.0
-        for k in reversed(range(1, DEGREE)):
+        for i in reversed(range(1, DEGREE)):
             sum_line = 0.0
-            for l in reversed(range(DEGREE)):
+            for j in reversed(range(DEGREE)):
                 sum_line *= latitude*latitude
-                sum_line += mat[2 * (k * DEGREE + l) + 1]
+                sum_line += mat[2 * (i * DEGREE + j) + 1]
             dgdx *= longitude*longitude
-            dgdx += sum_line * (2*k)
+            dgdx += sum_line * (2*i)
         dgdx *= longitude * latitude / cos_lat
 
         trace = (dfdx*dfdx + dfdy*dfdy + dgdx*dgdx + dgdy*dgdy) / 2
