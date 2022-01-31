54638a6880a8e9bf1b8ecf0c1bf067201a3b6ed9
trineutron <programgoo@gmail.com>
2022-01-31 18:07:42 +0900

    個々の点の誤差を関数に分割

--- src/polynomial.py
+++ src/polynomial.py
@@ -10,60 +10,64 @@ WEIGHT_S = 0.5
 
 
 @jit
+def calc_error_point(mat, longitude, latitude):
+    """Caltulate strain."""
+    latitude /= 2
+    cos_lat = math.cos(latitude * math.pi)
+
+    dfdx = 0.0
+    dgdy = 0.0
+    for i in range(DEGREE-1, -1, -1):
+        sum_dfdx = 0.0
+        sum_dgdy = 0.0
+        for j in range(DEGREE-1, -1, -1):
+            sum_dfdx *= latitude*latitude
+            sum_dgdy *= latitude*latitude
+            sum_dfdx += mat[2 * (i * DEGREE + j)]
+            sum_dgdy += mat[2 * (i * DEGREE + j) + 1] * (2*j+1)
+        dfdx *= longitude*longitude
+        dfdx += sum_dfdx * (2*i+1)
+        dgdy *= longitude*longitude
+        dgdy += sum_dgdy
+    dfdx /= cos_lat
+
+    dfdy = 0.0
+    for i in range(DEGREE-1, -1, -1):
+        sum_line = 0.0
+        for j in range(DEGREE-1, 0, -1):
+            sum_line *= latitude*latitude
+            sum_line += mat[2 * (i * DEGREE + j)] * (2*j)
+        dfdy *= longitude*longitude
+        dfdy += sum_line
+    dfdy *= longitude * latitude
+
+    dgdx = 0.0
+    for i in range(DEGREE-1, 0, -1):
+        sum_line = 0.0
+        for j in range(DEGREE-1, -1, -1):
+            sum_line *= latitude*latitude
+            sum_line += mat[2 * (i * DEGREE + j) + 1]
+        dgdx *= longitude*longitude
+        dgdx += sum_line * (2*i)
+    dgdx *= longitude * latitude / cos_lat
+
+    trace = (dfdx*dfdx + dfdy*dfdy + dgdx*dgdx + dgdy*dgdy) / 2
+    det = max(dfdx*dgdy - dfdy*dgdx, 1e-100)
+    ratio = (trace + math.sqrt(trace*trace - det*det)) / det
+    return cos_lat * (WEIGHT_S * math.log(det)**2
+                      + (1.0 - WEIGHT_S) * math.log(ratio)**2)
+
+
+@jit
 def calc_error(mat):
-    """Calculate strain."""
+    """Calculate sum of strain."""
     sum_error = 0.0
-    sum_weight = 0.0
-    for idx_longitude in range(2 * N):
+    for idx_longitude in range(N):
+        longitude = (idx_longitude + 0.5) / N
         for idx_latitude in range(N):
-            longitude = (idx_longitude + 0.5) / (2 * N)
-            latitude = (idx_latitude + 0.5) / (2 * N)
-            cos_lat = math.cos(latitude * math.pi)
-            weight = cos_lat
-            sum_weight += weight
-
-            dfdx = 0.0
-            dgdy = 0.0
-            for i in range(DEGREE-1, -1, -1):
-                sum_dfdx = 0.0
-                sum_dgdy = 0.0
-                for j in range(DEGREE-1, -1, -1):
-                    sum_dfdx *= latitude*latitude
-                    sum_dgdy *= latitude*latitude
-                    sum_dfdx += mat[2 * (i * DEGREE + j)]
-                    sum_dgdy += mat[2 * (i * DEGREE + j) + 1] * (2*j+1)
-                dfdx *= longitude*longitude
-                dfdx += sum_dfdx * (2*i+1)
-                dgdy *= longitude*longitude
-                dgdy += sum_dgdy
-            dfdx /= cos_lat
-
-            dfdy = 0.0
-            for i in range(DEGREE-1, -1, -1):
-                sum_line = 0.0
-                for j in range(DEGREE-1, 0, -1):
-                    sum_line *= latitude*latitude
-                    sum_line += mat[2 * (i * DEGREE + j)] * (2*j)
-                dfdy *= longitude*longitude
-                dfdy += sum_line
-            dfdy *= longitude * latitude
-
-            dgdx = 0.0
-            for i in range(DEGREE-1, 0, -1):
-                sum_line = 0.0
-                for j in range(DEGREE-1, -1, -1):
-                    sum_line *= latitude*latitude
-                    sum_line += mat[2 * (i * DEGREE + j) + 1]
-                dgdx *= longitude*longitude
-                dgdx += sum_line * (2*i)
-            dgdx *= longitude * latitude / cos_lat
-
-            trace = (dfdx*dfdx + dfdy*dfdy + dgdx*dgdx + dgdy*dgdy) / 2
-            det = max(dfdx*dgdy - dfdy*dgdx, 1e-100)
-            ratio = (trace + math.sqrt(trace*trace - det*det)) / det
-            sum_error += weight * (WEIGHT_S * math.log(det)**2
-                                   + (1.0 - WEIGHT_S) * math.log(ratio)**2)
-    return sum_error / sum_weight
+            latitude = (idx_latitude + 0.5) / N
+            sum_error += calc_error_point(mat, longitude, latitude)
+    return sum_error / N**2 * math.pi / 2
 
 
 init = [0] * (2*DEGREE*DEGREE)
